acf04116f8fd210096d1d4bc714dd5134d3def1b
Vinícius dos Santos Tavares <47855624+vini1lol@users.noreply.github.com>
2019-10-28 19:31:44 -0300

    up

--- mqtt/lib/main.dart
+++ mqtt/lib/main.dart
@@ -56,14 +56,14 @@ class _HomepageState extends State<Homepage> {
         .startClean() // Non persistent session for testing
         .withWillQos(MqttQos.atLeastOnce);
     setState(() {
-      p.add('EXAMPLE::Mosquitto client connecting....');
+      p.add('Mosquitto client connecting....');
     });
     client.connectionMessage = connMess;
     try {
       await client.connect();
     } on Exception catch (e) {
       setState(() {
-        p.add('EXAMPLE::client exception - $e');
+        p.add('client exception - $e');
       });
       client.disconnect();
     }
@@ -71,71 +71,71 @@ class _HomepageState extends State<Homepage> {
     /// Check we are connected
     if (client.connectionStatus.state == MqttConnectionState.connected) {
       setState(() {
-        p.add('EXAMPLE::Mosquitto client connected');
+        p.add('Mosquitto client connected');
       });
     } else {
       /// Use status here rather than state if you also want the broker return code.
       setState(() {
         p.add(
-            'EXAMPLE::ERROR Mosquitto client connection failed - disconnecting, status is ${client.connectionStatus}');
+            'ERROR Mosquitto client connection failed - disconnecting, status is ${client.connectionStatus}');
       });
       client.disconnect();
       exit(-1);
     }
 
     /// Ok, lets try a subscription
-    setState(() {
-      p.add('EXAMPLE::Subscribing to the test/lol topic');
-    });
-    const String topic = 'test/lol'; // Not a wildcard topic
-    client.subscribe(topic, MqttQos.atMostOnce);
+    // setState(() {
+    //   p.add('EXAMPLE::Subscribing to the test/lol topic');
+    // });
+    // const String topic = 'test/lol'; // Not a wildcard topic
+    // client.subscribe(topic, MqttQos.atMostOnce);
 
     /// The client has a change notifier object(see the Observable class) which we then listen to to get
     /// notifications of published updates to each subscribed topic.
-    client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
-      final MqttPublishMessage recMess = c[0].payload;
-      final String pt =
-          MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+    // client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+    //   final MqttPublishMessage recMess = c[0].payload;
+    //   final String pt =
+    //       MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
 
-      /// The above may seem a little convoluted for users only interested in the
-      /// payload, some users however may be interested in the received publish message,
-      /// lets not constrain ourselves yet until the package has been in the wild
-      /// for a while.
-      /// The payload is a byte buffer, this will be specific to the topic
-      setState(() {
-        p.add('Notification: topic is <${c[0].topic}>, payload is <-- $pt -->');
-        p.add('');
-      });
-    });
+    /// The above may seem a little convoluted for users only interested in the
+    /// payload, some users however may be interested in the received publish message,
+    /// lets not constrain ourselves yet until the package has been in the wild
+    /// for a while.
+    /// The payload is a byte buffer, this will be specific to the topic
+    //   setState(() {
+    //     p.add('Notification: topic is <${c[0].topic}>, payload is <-- $pt -->');
+    //     p.add('');
+    //   });
+    // });
 
     /// If needed you can listen for published messages that have completed the publishing
     /// handshake which is Qos dependant. Any message received on this stream has completed its
     /// publishing handshake with the broker.
-    client.published.listen((MqttPublishMessage message) {
-      setState(() {
-        p.add(
-            'EXAMPLE::Published notification:: topic is ${message.variableHeader.topicName}, with Qos ${message.header.qos}');
-      });
-    });
+    // client.published.listen((MqttPublishMessage message) {
+    //   setState(() {
+    //     p.add(
+    //         'EXAMPLE::Published notification:: topic is ${message.variableHeader.topicName}, with Qos ${message.header.qos}');
+    //   });
+    // });
 
     /// Lets publish to our topic
     /// Use the payload builder rather than a raw buffer
     /// Our known topic to publish to
-    const String pubTopic = 'Dart/Mqtt_client/testtopic';
-    final MqttClientPayloadBuilder builder = MqttClientPayloadBuilder();
-    builder.addString('Hello/ola');
+    // const String pubTopic = 'Dart/Mqtt_client/testtopic';
+    // final MqttClientPayloadBuilder builder = MqttClientPayloadBuilder();
+    // builder.addString('Hello/ola');
 
     /// Subscribe to it
-    setState(() {
-      p.add('EXAMPLE::Subscribing to the Dart/Mqtt_client/testtopic topic');
-    });
-    client.subscribe(pubTopic, MqttQos.exactlyOnce);
+    // setState(() {
+    //   p.add('EXAMPLE::Subscribing to the Dart/Mqtt_client/testtopic topic');
+    // });
+    // client.subscribe(pubTopic, MqttQos.exactlyOnce);
 
     /// Publish it
-    setState(() {
-      p.add('EXAMPLE::Publishing our topic');
-    });
-    client.publishMessage(pubTopic, MqttQos.exactlyOnce, builder.payload);
+    // setState(() {
+    //   p.add('EXAMPLE::Publishing our topic');
+    // });
+    // client.publishMessage(pubTopic, MqttQos.exactlyOnce, builder.payload);
 
     /// Ok, we will now sleep a while, in this gap you will see ping request/response
     /// messages being exchanged by the keep alive mechanism.
@@ -162,20 +162,18 @@ class _HomepageState extends State<Homepage> {
   /// The subscribed callback
   void onSubscribed(String topic) {
     setState(() {
-      widget.ps.add('EXAMPLE::Subscription confirmed for topic $topic');
+      widget.ps.add('Subscription confirmed for topic $topic');
     });
   }
 
   /// The unsolicited disconnect callback
   void onDisconnected() {
     setState(() {
-      widget.ps.add(
-          'EXAMPLE::OnDisconnected client callback - Client disconnection');
+      widget.ps.add('OnDisconnected client callback - Client disconnection');
     });
     if (client.connectionStatus.returnCode == MqttConnectReturnCode.solicited) {
       setState(() {
-        widget.ps.add(
-            'EXAMPLE::OnDisconnected callback is solicited, this is correct');
+        widget.ps.add('OnDisconnected callback is solicited, this is correct');
       });
     }
     exit(-1);
@@ -183,13 +181,13 @@ class _HomepageState extends State<Homepage> {
 
   /// The successful connect callback
   void onConnected() {
-    widget.ps.add(
-        'EXAMPLE::OnConnected client callback - Client connection was sucessful');
+    widget.ps
+        .add('OnConnected client callback - Client connection was sucessful');
   }
 
   /// Pong callback
   void pong() {
-    widget.ps.add('EXAMPLE::Ping response client callback invoked');
+    widget.ps.add('Ping response client callback invoked');
   }
 
   accelerometerEvent(AccelerometerEvent event) {
@@ -197,7 +195,7 @@ class _HomepageState extends State<Homepage> {
     setState(() {
       widget.ps.add(event);
     });
-    String topic = "Accele";
+    String topic = widget.data;
     final MqttClientPayloadBuilder bd = MqttClientPayloadBuilder();
     bd.addString('Hello from mqtt_client');
     client.publishMessage(topic, MqttQos.exactlyOnce, bd.payload);
@@ -213,18 +211,33 @@ class _HomepageState extends State<Homepage> {
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.play_arrow),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/second', arguments: "hi");
-              },
-            )
+              onPressed: processo,
+            ),
+            IconButton(
+                icon: Icon(Icons.settings),
+                onPressed: () {
+                  Navigator.of(context).pushNamed("/");
+                }),
           ],
         ),
         body: Column(
           children: <Widget>[
-            new Text(
-              "x =${widget.x}, Y=${widget.y} , Z=${widget.z}  ",
-              style: TextStyle(
-                fontSize: 30,
+            new Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "x =${widget.x}, Y=${widget.y} , Z=${widget.z}  ",
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            new Padding(
+              padding: EdgeInsets.all(9.0),
+              child: Text(
+                "MqttTopic: ${widget.data}",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
             ),
             new Expanded(
@@ -232,7 +245,12 @@ class _HomepageState extends State<Homepage> {
                 itemCount: widget.ps.length,
                 itemBuilder: (BuildContext ctxt, int index) {
                   final item = widget.ps[index];
-                  return Text(item);
+                  return Text(
+                    item,
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  );
                 },
               ),
             ),
--- mqtt/lib/mqttdetails.dart
+++ mqtt/lib/mqttdetails.dart
@@ -1,23 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class Mqttdetail extends StatelessWidget {
+class Mqttdetail extends StatefulWidget {
+  @override
+  _MqttdetailState createState() => _MqttdetailState();
+}
+
+class _MqttdetailState extends State<Mqttdetail> {
+  final myController = TextEditingController();
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Mqtt Settings"),
-        ),
-        body: Column(
-          children: <Widget>[
-            new Text("Mqtt topic"),
-            new TextField(
+      appBar: AppBar(
+        title: Text("Mqtt Settings"),
+      ),
+      body: Column(
+        children: <Widget>[
+          new Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Mqtt topic",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+          new Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: "topic",
               ),
-            )
-          ],
-        ));
+              controller: myController,
+            ),
+          ),
+          new RaisedButton(
+            child: Text("Submit"),
+            onPressed: () {
+              Navigator.of(context).pushNamed(
+                "/second",
+                arguments: myController.text,
+              );
+            },
+          )
+        ],
+      ),
+    );
   }
 }
